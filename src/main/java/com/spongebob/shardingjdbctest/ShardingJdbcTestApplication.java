c8925012309acc1105ac01dd92245566881c023c
SpongeBob <374506395@qq.com>
2024-06-29 02:15:53 -0700

    init

---
+++ src/main/java/com/spongebob/shardingjdbctest/ShardingJdbcTestApplication.java
@@ -0,0 +1,13 @@
+package com.spongebob.shardingjdbctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcTestApplication.class, args);
+    }
+
+}
